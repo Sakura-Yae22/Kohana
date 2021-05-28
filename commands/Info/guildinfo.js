9ff22c1ddd4bfc7604d27b3aac73d2676465d6a5
numselli <58607232+numselli@users.noreply.github.com>
2021-05-28 14:28:33 -0700

    add more info commands

---
+++ commands/Info/guildinfo.js
@@ -0,0 +1,68 @@
+module.exports = {
+    "commandLogic": async function commandLogic(itemsToImport) {
+        const {message} = itemsToImport;
+
+        const guild = message.channel.guild;
+
+        message.channel.createMessage({
+            "embed": {
+              "title": guild.name,
+              "color": 5747894,
+              "timestamp": new Date().toISOString(),
+              "thumbnail": {
+                "url":  guild.iconURL || 'https://cdn.discordapp.com/embed/avatars/4.png'
+              },
+              "fields": [
+                {
+                    "name": "ID",
+                    "value": `\`${guild.id}\``,
+                    "inline": true
+                },
+                {
+                  "name": "Icon",
+                  "value": `[URL](${guild.iconURL  || "https://cdn.discordapp.com/embed/avatars/4.png"})`,
+                  "inline": true
+                },
+                {
+                  "name": "Owner",
+                  "value": `<@${guild.ownerID}>`,
+                  "inline": true
+                },
+                {
+                  "name": "Region",
+                  "value": `\`${guild.region}\``,
+                  "inline": true
+                },
+                {
+                  "name": "Roles",
+                  "value": `${guild.roles.size || 0}`,
+                  "inline": true
+                },
+                {
+                    "name": "Categories",
+                    "value": `${guild.channels.filter(channel => channel.type== 4).lenght || 0}`,
+                    "inline": true
+                },
+                {
+                    "name": "Text Channels",
+                    "value": `${guild.channels.filter(channel => channel.type== 0).lenght || 0}`,
+                    "inline": true
+                },
+                {
+                    "name": "Voice Channels",
+                    "value": `${guild.channels.filter(channel => channel.type== 2).lenght || 0}`,
+                    "inline": true
+                },
+                {
+                    "name": "Members",
+                    "value": `${guild.memberCount}`,
+                    "inline": true
+                }
+              ]
+            }
+        }).catch(err => console.error("Cannot send messages to this channel", err));
+    },
+    "help":[
+        {"name": "__Usage__","value": "Shows info about the about a guild member.\n```??botPrefix??userinfo```"}
+    ]
+}
