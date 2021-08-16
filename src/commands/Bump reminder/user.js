4e41979698023c177818d097af5aa20a4a724701
numselli <58607232+numselli@users.noreply.github.com>
2021-08-16 15:47:00 -0700

    improve bump reminder user command

--- src/commands/Bump reminder/user.js
+++ src/commands/Bump reminder/user.js
@@ -1,11 +1,8 @@
 module.exports = {
-    "commandLogic": async function commandLogic(itemsToImport) {
-        const {message, sharder} = itemsToImport;
-        const userid = message.mentions[0]?.id || message.content.split(" ")[0] || message.author.id;
-        let total = 0;
-        await (await sharder.query({text: 'SELECT * FROM users WHERE userid = $1', values: [userid]})).map(({bumps})=>{total += parseInt(bumps)});
+    "commandLogic": async function commandLogic({message, sharder} = itemsToImport) {
+        const totalBumps = await sharder.query({text: 'SELECT SUM(bumps) FROM users WHERE userid = $1', values: [message.mentions[0]?.id || message.content.split(" ")[0] || message.author.id]});
 
-        message.channel.createMessage( {"embed": {"title": `User stats`,"description": `Total bumps: **${total}**`, "color": 5747894,"timestamp": new Date().toISOString()}}).catch(err => console.error("Cannot send messages to this channel", err));
+        message.channel.createMessage({"embed": {"title": `User stats`,"description": `Total bumps: **${totalBumps[0].sum}**`, "color": 5747894}}).catch(err => console.error("Cannot send messages to this channel", err));
     },
     "help":[
         {"name": "__Usage__","value": "Shows a users total bumps.\n```??botPrefix??user```\n```??botPrefix??user @ USER```\n```??botPrefix??user <USER_ID>```"}
