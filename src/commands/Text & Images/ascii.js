b8e8e26eddf2d5eb5077cb9f43017e8f46d5e958
numselli <58607232+numselli@users.noreply.github.com>
2021-08-17 17:21:38 -0700

    update ascii command

--- src/commands/Text & Images/ascii.js
+++ src/commands/Text & Images/ascii.js
@@ -1,9 +1,11 @@
-const ascii = require('ascii-art');
+const figlet = require('figlet');
 module.exports = {
     "commandLogic": async function commandLogic(itemsToImport){
         const {message}=itemsToImport;
-        const asciiText = "```" + await ascii.font(message.content, 'doom');
-        message.channel.createMessage( (asciiText.length > 2000) ? 'The ASCII art is too big.' : asciiText+"```").catch(err => console.error("Cannot send messages to this channel", err));
+        figlet(message.content, function(err, ascii) {
+            if (err) return message.channel.createMessage("error making ASCII art").catch(err => console.error("Cannot send messages to this channel", err));
+            message.channel.createMessage( (ascii.length+2 > 2000) ? 'The ASCII art is too big.' : "```"+ascii+"```").catch(err => console.error("Cannot send messages to this channel", err));
+        });
     },
     "help":[
         {"name": "__Usage__","value": "Converts text to ASCII art.\n```\n??botPrefix??ascii <TEXT>\n```","inline": true}
