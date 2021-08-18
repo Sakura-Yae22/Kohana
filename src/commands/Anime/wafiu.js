4c3e2a02230c408c43f6c61a3f767e839635fdd5
numselli <58607232+numselli@users.noreply.github.com>
2021-08-18 16:02:58 -0700

    add wafiu command (suggested by ACHIRA GAMING)

---
+++ src/commands/Anime/wafiu.js
@@ -0,0 +1,18 @@
+module.exports = {
+    "commandLogic": async function commandLogic(itemsToImport) {
+        const {message, sharder} = itemsToImport;
+
+        const nekoJSON = await sharder.nekoslife.sfw.waifu();
+
+        message.channel.createMessage({"embed": {
+        "title": "Waifu",
+            "color": 2717868,
+            "image": {
+                "url": nekoJSON.url
+            }
+        }}).catch(err => console.error("Cannot send messages to this channel", err));
+    },
+    "help":[
+        {"name": "__Usage__","value": "Shows a random waifu image.\n```\n??botPrefix??wafiu\n```","inline": true}
+    ]
+};
