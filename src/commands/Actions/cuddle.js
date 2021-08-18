c8da96770ae888c9899c0f6c6f24401d818b4f34
numselli <58607232+numselli@users.noreply.github.com>
2021-08-18 16:33:00 -0700

    merge ACHIRA GAMING's cuddle command in

--- src/commands/Actions/cuddle.js
+++ src/commands/Actions/cuddle.js
@@ -1,20 +1,22 @@
 const fetch = require('node-fetch');
 module.exports = {
     "commandLogic": async function commandLogic(itemsToImport) {
-        const {message} = itemsToImport;
+        const {message, sharder} = itemsToImport;
 
         if (message.mentions.length !== 1) return  message.channel.createMessage("Please mention a user.").catch(err => console.error("Cannot send messages to this channel", err));
         if (message.mentions[0].bot) return  message.channel.createMessage("You cant cuddle a bot.").catch(err => console.error("Cannot send messages to this channel", err));
         if (message.mentions[0].id === message.author.id) return  message.channel.createMessage("You cuddle your self.").catch(err => console.error("Cannot send messages to this channel", err));
 
-        const cuddle = await fetch('https://purrbot.site/api/img/sfw/cuddle/gif');
-        const cuddleJSON = await cuddle.json();
+        const ranChance = Number((Math.random() * 1).toFixed(1));
+
+        const cuddle = ranChance>=0.5 ? await (await fetch('https://purrbot.site/api/img/sfw/cuddle/gif')).json() : await sharder.nekoslife.sfw.cuddle();
+        
         message.channel.createMessage({
             "embed": {
                 "title": `${message.mentions[0].username} was cuddled by ${message.author.username}`,
                 "color": 2717868,
                 "image": {
-                    "url": cuddleJSON.link
+                    "url": cuddle[ranChance>=0.5 ? "link" : "url"]
                 }
             }
         }).catch(err => console.error("Cannot send messages to this channel", err));
