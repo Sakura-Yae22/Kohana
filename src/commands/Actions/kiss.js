d3a3250c60748f3bb91c9c3a4efb6a9097e4ac58
numselli <58607232+numselli@users.noreply.github.com>
2021-08-18 20:23:44 -0700

    merge ACHIRA GAMING's kiss command in

--- src/commands/Actions/kiss.js
+++ src/commands/Actions/kiss.js
@@ -6,7 +6,8 @@ module.exports = {
         if (message.mentions.length !== 1) return message.channel.createMessage("Please mention a user.").catch(err => console.error("Cannot send messages to this channel", err));
         if (message.mentions[0].bot) return  message.channel.createMessage("You cant kiss a bot.").catch(err => console.error("Cannot send messages to this channel", err));
         if (message.mentions[0].id === message.author.id) return  message.channel.createMessage("You cant kiss your self.").catch(err => console.error("Cannot send messages to this channel", err));
-        const kiss = await fetch('https://purrbot.site/api/img/sfw/kiss/gif');
+        
+        const kiss = await fetch((nekosLifeRandChance >= 0.5) ? 'https://purrbot.site/api/img/sfw/kiss/gif' : "https://anime-api.hisoka17.repl.co/img/kiss");
         const kissJSON = await kiss.json();
 
         message.channel.createMessage({
