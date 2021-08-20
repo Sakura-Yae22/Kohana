e84b37b5ec82c0f5b7c843e8f2bed0d51a05b0cf
numselli <58607232+numselli@users.noreply.github.com>
2021-08-20 13:55:09 -0700

    add some info commands

---
+++ src/slashCommands/Info/guildinfo.js
@@ -0,0 +1,64 @@
+module.exports.commandLogic = async itemsToImport => {
+  const { interaction } = itemsToImport;
+
+  const guild = interaction.member.guild;
+
+  interaction.createMessage({
+    "embeds": [{
+      "title": guild.name,
+      "color": 5747894,
+      "thumbnail": {
+        "url": guild.iconURL ?? 'https://cdn.discordapp.com/embeds/avatars/4.png'
+      },
+      "fields": [
+        {
+          "name": "ID",
+          "value": `\`${guild.id}\``,
+          "inline": true
+        },
+        {
+          "name": "Icon",
+          "value": `[URL](${guild.iconURL ?? "https://cdn.discordapp.com/embeds/avatars/4.png"})`,
+          "inline": true
+        },
+        {
+          "name": "Owner",
+          "value": `<@${guild.ownerID}>`,
+          "inline": true
+        },
+        {
+          "name": "Region",
+          "value": `\`${guild.region}\``,
+          "inline": true
+        },
+        {
+          "name": "Roles",
+          "value": `${guild.roles.size ?? 0}`,
+          "inline": true
+        },
+        {
+          "name": "Categories",
+          "value": `${guild.channels.filter(channel => channel.type == 4).length ?? 0}`,
+          "inline": true
+        },
+        {
+          "name": "Text Channels",
+          "value": `${guild.channels.filter(channel => channel.type == 0).length ?? 0}`,
+          "inline": true
+        },
+        {
+          "name": "Voice Channels",
+          "value": `${guild.channels.filter(channel => channel.type == 2).length ?? 0}`,
+          "inline": true
+        },
+        {
+          "name": "Members",
+          "value": `${guild.memberCount}`,
+          "inline": true
+        }
+      ]
+    }]
+  }).catch(err => console.error("Cannot send messages to this channel", err));
+}
+
+module.exports.description = "Shows info about the about the guild"
