0c7d9a721f07609a7d3476d5355b726a39c96dc2
numselli <58607232+numselli@users.noreply.github.com>
2021-08-19 22:51:23 -0700

    remove debug code

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -1,5 +1,4 @@
 module.exports = (sharder, interaction) => {
-    console.log(JSON.stringify(interaction))
     if (!sharder.bot.slashCommands.has(interaction.data.name)) return;
     sharder.bot.slashCommands.get(interaction.data.name)({interaction, sharder});
 }
