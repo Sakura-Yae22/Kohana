3bcf8689651127b5d6729b7c3fe5d690348c9c14
numselli <58607232+numselli@users.noreply.github.com>
2021-08-19 23:07:59 -0700

    fix tail command

--- src/slashCommands/Anime/tail.js
+++ src/slashCommands/Anime/tail.js
@@ -5,15 +5,15 @@ module.exports.commandLogic = async itemsToImport => {
 
     const tail = await fetch('https://purrbot.site/api/img/sfw/tail/gif');
     const tailJSON = await tail.json();
-    interaction.createMessage([{
-        "embeds": {
+    interaction.createMessage({
+        "embeds": [{
             "title": `TAIL`,
             "color": 2717868,
             "image": {
                 "url": tailJSON.link
             }
-        }
-    }]).catch(err => console.error("Cannot send messages to this channel", err));
+        }]
+    }).catch(err => console.error("Cannot send messages to this channel", err));
 }
 
 module.exports.description = "Wag your tail"
