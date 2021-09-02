c372f73c23aaa514b8c5d5f80056454accf76c7f
numselli <58607232+numselli@users.noreply.github.com>
2021-09-01 18:15:39 -0700

    update info command

--- src/slashCommands/Info/info.js
+++ src/slashCommands/Info/info.js
@@ -1,3 +1,5 @@
+const {MaxShards} = require('../../static/config.json');
+
 module.exports.commandLogic = async itemsToImport => {
     const {sharder, interaction} = itemsToImport;
 
@@ -9,7 +11,7 @@ module.exports.commandLogic = async itemsToImport => {
                 "fields": [
                     {
                         "name": "Bot",
-                        "value": `**Shard Ping: ** ${interaction.member.guild.shard.latency} MS\n**Current shard: **${sharder.bot.shards.get(sharder.bot.shards.keys().next().value).id}\n**Total shards: **${sharder.config.MaxShards}\n**Uptime: **${(Math.floor(sharder.bot.uptime / (1000 * 60 * 60 * 24))).toFixed(0)} Day(s) ${(Math.floor((sharder.bot.uptime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))).toFixed(0)}:${(Math.floor((sharder.bot.uptime % (1000 * 60 * 60)) / (1000 * 60))).toFixed(0)}:${(Math.floor((sharder.bot.uptime % (1000 * 60)) / 1000)).toFixed(0)}`,
+                        "value": `**Shard Ping: ** ${interaction.member.guild.shard.latency} MS\n**Current shard: **${sharder.bot.shards.get(sharder.bot.shards.keys().next().value).id}\n**Total shards: **${MaxShards}\n**Uptime: **${(Math.floor(sharder.bot.uptime / (1000 * 60 * 60 * 24))).toFixed(0)} Day(s) ${(Math.floor((sharder.bot.uptime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))).toFixed(0)}:${(Math.floor((sharder.bot.uptime % (1000 * 60 * 60)) / (1000 * 60))).toFixed(0)}:${(Math.floor((sharder.bot.uptime % (1000 * 60)) / 1000)).toFixed(0)}`,
                         "inline": false
                     },
                     {
