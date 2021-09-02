704b7d285c792054cf123014a7bf3a6cea684cb6
numselli <58607232+numselli@users.noreply.github.com>
2021-09-01 20:35:58 -0700

    Update user.js

--- src/slashCommands/Bump reminder/user.js
+++ src/slashCommands/Bump reminder/user.js
@@ -1,9 +1,7 @@
 module.exports.commandLogic = async itemsToImport => {
     const {interaction, sharder} = itemsToImport;
 
-    const mentionedUserID = interaction.data.options?.[0].value ?? interaction.member.id
-    const totalBumps = await sharder.ipc.command("db", {text: 'SELECT SUM(bumps) FROM users WHERE userid = $1', values: [ mentionedUserID ]}, true);
-
+    const totalBumps = await sharder.ipc.command("db", {text: 'SELECT SUM(bumps) FROM users WHERE userid = $1', values: [ interaction.data.options?.[0].value ?? interaction.member.id ]}, true);
     interaction.createMessage({"embeds": [{"title": `User stats`,"description": `Total bumps: **${totalBumps[0].sum}**`, "color": 5747894}]}).catch(err => console.error("Cannot send messages to this channel", err));
 }
 
