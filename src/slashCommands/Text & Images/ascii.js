963cdb9f0e315300f1194e6d40bc47fde5623408
numselli <58607232+numselli@users.noreply.github.com>
2021-09-01 22:37:58 -0700

    add ascii command back in

---
+++ src/slashCommands/Text & Images/ascii.js
@@ -0,0 +1,20 @@
+const figlet = require('figlet');
+module.exports.commandLogic = async itemsToImport => {
+    const { interaction } = itemsToImport;
+    figlet(interaction.data.options[0].value, function (err, ascii) {
+        if (err) return interaction.createMessage({"flags":64, "content": "error making ASCII art"}).catch(err => console.error("Cannot send messages to this channel", err));
+        interaction.createMessage((ascii.length + 2 > 2000) ? 'The ASCII art is too big.' : "```" + ascii + "```").catch(err => console.error("Cannot send messages to this channel", err));
+    });
+}
+
+module.exports.description = "Converts text to ASCII art"
+
+
+module.exports.options = [
+    {
+      "name": "text",
+      "description": "The text to convert to ASCII art",
+      "type": 3,
+      "required": true,
+    }
+]
