e10cdb169ab74bfeb4f635a32afc5c96447ae1f9
numselli <58607232+numselli@users.noreply.github.com>
2021-10-30 03:18:20 -0700

    Update makeserverLB.mjs

--- src/utils/makeserverLB.mjs
+++ src/utils/makeserverLB.mjs
@@ -1,12 +1,9 @@
 import addPadding from '../utils/addPadding.mjs'
 
 export default async(table)=>{    
-    const longestUserNameLength = [...table].sort((a, b) => b.username.length - a.username.length)[0].username.length;
-    
-    const userNamesObj = {};
-    table.map(user => {userNamesObj[user.userid]=user.username})
+    const longestUserNameLength = table.reduce((acc, { username }) => Math.max(acc, username.length), 0);
             
-    const userList = table.map((user, index) => ` | ${(index+1)}${(index+1).toString().length==1 ? "  " : " " }| ${userNamesObj[user.userid]}${addPadding(longestUserNameLength-userNamesObj[user.userid].length, " ")} | ${user.bumps}${addPadding(table[0].bumps.toString().length - user.bumps.toString().length, " ")} |`)
+    const userList = table.map((user, index) => ` | ${(index+1)}${(index+1).toString().length==1 ? "  " : " " }| ${user.username}${addPadding(longestUserNameLength-(user.username.length), " ")} | ${user.bumps}${addPadding(table[0].bumps.toString().length - user.bumps.toString().length, " ")} |`)
     
     const userPadding = addPadding(longestUserNameLength - 4, " ");
     const bumpsPadding = addPadding(table[0].bumps.toString().length - 3, " ");
