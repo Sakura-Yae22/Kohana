320bb5419c43391c0a99f8145ab53308133048d4
numselli <58607232+numselli@users.noreply.github.com>
2021-10-30 00:27:05 -0700

    Update help.mjs

--- src/slashCommands/help.mjs
+++ src/slashCommands/help.mjs
@@ -1,28 +1,29 @@
 export const commandLogic = async itemsToImport => {
     const {interaction, sharder} = itemsToImport
-    // const help = {
-    //     "embeds": [
-    //         {
-    //             "title": "Help",
-    //             "description": `Below is a list of my commands. For further help you can join the [support server](${(await sharder.ipc.command("db", {text: 'SELECT * FROM links WHERE id = $1', values: ['support']}, true))[0].value}`,
-    //             "fields": [],
-    //             "color": 2717868,
-    //         }
-    //     ]
-    // }
+    const help = {
+        "embeds": [
+            {
+                "title": "Help",
+                "description": `Below is a list of my commands. For further help you can join the [support server](${(await sharder.ipc.command("db", {text: 'SELECT * FROM links WHERE id = $1', values: ['support']}, true))[0].value})`,
+                "fields": [],
+                "color": 2717868,
+            }
+        ]
+    }
 
-    console.log(await sharder.ipc.command("db", {text: 'SELECT * FROM links WHERE id = $1', values: ['support']}, true))
-
+    const ownerID = (await sharder.bot.getOAuthApplication()).owner.id
     const slashCommand = await sharder.bot.getCommands();
-    slashCommand.map(async command => {
-        const {category} = await import(`./${command.name}.mjs`)
-        const catagoryIndex = help.embeds[0].fields.findIndex(element => element.name === category);
+    await Promise.all(
+        slashCommand.map(async command => {
+            const {category} = await import(`./${command.name}.mjs`);
+            if (!(interaction.member.id === ownerID) && category === "Owner") return;
 
-        if (catagoryIndex == -1) help.embeds[0].fields.push({ "name": category, "value": `\`\`\`\n${command.name}\n`, "inline": true })
-        else help.embeds[0].fields[catagoryIndex].value += `${command.name}\n`
-    })
+            const catagoryIndex = help.embeds[0].fields.findIndex(element => element.name === category);
 
-    help.embeds[0].fields.map(field=>field.value+="```")
+            if (catagoryIndex == -1) help.embeds[0].fields.push({ "name": category, "value": `\`\`\`\n${command.name}\n\`\`\``, "inline": true })
+            else help.embeds[0].fields[catagoryIndex].value = `${help.embeds[0].fields[catagoryIndex].value.slice(0, -3)}${command.name}\n${help.embeds[0].fields[catagoryIndex].value.slice(-3)}`;  
+        })
+    )
 
     interaction.createMessage(help).catch(err => console.error("Cannot send messages to this channel"));
 }
@@ -30,3 +31,5 @@ export const commandLogic = async itemsToImport => {
 export const description = "Lists all commands"
 
 export const category = "Info" 
+
+
