4a887648e0d780063c8c562b7cd3c20cd5f93e84
numselli <58607232+numselli@users.noreply.github.com>
2021-10-29 23:32:06 -0700

    Update pat.mjs

--- src/slashCommands/pat.mjs
+++ src/slashCommands/pat.mjs
@@ -1,6 +1,4 @@
 import fetch from 'node-fetch'
-import nekosDotLife from "nekos.life";
-const nekoslife = new nekosDotLife();
 
 export const commandLogic = async itemsToImport => {
     const {interaction} = itemsToImport;
@@ -9,15 +7,14 @@ export const commandLogic = async itemsToImport => {
     const mentionedUserID = interaction.data.resolved.users.keys().next().value
     if (mentionedUserID === interaction.member.user.id) return interaction.createMessage({"flags":64, "content": "You cant pat yourself."}).catch(err => console.error("Cannot send messages to this channel", err));
 
-    const ranChance = Number((Math.random() * 1).toFixed(1));
-    const pat = ranChance>=0.5 ? await (await fetch('https://purrbot.site/api/img/sfw/pat/gif')).json() : await nekoslife.sfw.pat();
-    
+    const pat = await (await fetch('https://purrbot.site/api/img/sfw/pat/gif')).json()
+
     interaction.createMessage({
         "embeds": [{
             "title": `${interaction.data.resolved.users.get(mentionedUserID).username} was patted by ${interaction.member.user.username}`,
             "color": 2717868,
             "image": {
-                "url": pat[ranChance>=0.5 ? "link" : "url"]
+                "url": pat.link
             }
         }]
     }).catch(err => console.error("Cannot send messages to this channel", err));
