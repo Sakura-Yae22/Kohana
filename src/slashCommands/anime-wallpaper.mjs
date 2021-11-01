f936e7b1c630e46e8f67d3c877aa72594105f8d8
numselli <58607232+numselli@users.noreply.github.com>
2021-11-01 13:11:12 -0700

    Update anime-wallpaper.mjs

--- src/slashCommands/anime-wallpaper.mjs
+++ src/slashCommands/anime-wallpaper.mjs
@@ -1,16 +1,18 @@
-import nekosDotLife from "nekos.life";
-const nekoslife = new nekosDotLife();
+import fetch from 'node-fetch'
 
 export const commandLogic = async itemsToImport => {
     const {interaction} = itemsToImport;
     
-    const wallpaper = await nekoslife.sfw.wallpaper();
+    const wall = await fetch('https://wallhaven.cc/api/v1/search?categories=010&sorting=random')
+    const wallJSON = await wall.json()
 
+    wallJSON.data[Math.floor(Math.random() * wallJSON.data.length)].path
+   
     interaction.createMessage({"embeds": [{
     "title": "Wallpaper",
         "color": 2717868,
         "image": {
-            "url": wallpaper.url
+            "url": wallJSON.data[Math.floor(Math.random() * wallJSON.data.length)].path
         }
     }]}).catch(err => console.error("Cannot send messages to this channel", err));
 }
