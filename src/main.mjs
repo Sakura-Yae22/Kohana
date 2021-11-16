d79f7c82ec298c17e863189970cf5cece6feadf3
numselli <58607232+numselli@users.noreply.github.com>
2021-11-15 16:16:53 -0800

    Update main.mjs

--- src/main.mjs
+++ src/main.mjs
@@ -3,6 +3,7 @@ import { BaseClusterWorker } from 'eris-fleet';
 import Eris from "eris";
 import cron from 'node-cron';
 import checkExpiredservers from './utils/checkExpiredservers.mjs';
+import fs from 'fs/promises';
 
 class BotWorker extends BaseClusterWorker {
     constructor(setup) {
@@ -10,8 +11,8 @@ class BotWorker extends BaseClusterWorker {
 
         (async () => {
             // checkExpiredservers
-            cron.schedule("0 0 * * *", () => { checkExpiredservers(this, true) });
-            cron.schedule("* * * * *", () => { checkExpiredservers(this, false) });
+            cron.schedule("0 0 * * *", () => {checkExpiredservers(this, true)});
+            cron.schedule("* * * * *", () => {checkExpiredservers(this, false)});
 
             // init events
             const events = ["interactionCreate", "messageCreate"];
@@ -20,6 +21,18 @@ class BotWorker extends BaseClusterWorker {
                 this.bot.on(event, eventFuntion.default.bind(null, this));
                 console.log(`[Event Loaded] ${event}`);
             })
+
+            const names = (await this.bot.getCommands()).map(registeredCMD => registeredCMD.name)
+            const slashCommands = await fs.readdir('slashCommands');
+            slashCommands.filter(name => name.endsWith(".mjs"));
+            slashCommands.map(async slashCommand => {
+                const name = slashCommand.split(".mjs")[0];
+                console.log(`[Slash Command Loaded] ${name}`);
+                
+                if (names.includes(name)) return;
+                const {description, options} = await import(`./slashCommands/${slashCommand}`);
+                this.bot.createCommand({name, description, options, type: 1});
+            });
         })();
     }
 
